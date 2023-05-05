6c284151cf9cfbabf4dc3458dd1a19dd52f85ce5
Saeed Rasooli <saeed.gnu@gmail.com>
2023-05-05 12:13:17 +0330

    add search_utils/similarity_test.go

---
+++ search_utils/similarity_test.go
@@ -0,0 +1,29 @@
+package search_utils
+
+import (
+	"testing"
+
+	"github.com/ilius/go-dict-commons/levenshtein"
+)
+
+func distance(a string, b string, buff []uint16) int {
+	return levenshtein.ComputeDistance([]rune(a), []rune(b))
+}
+
+func TestSimilarity1(t *testing.T) {
+	buff := make([]uint16, 100)
+	test := func(s1 string, s2 string, score uint8) {
+		actualScore := Similarity([]rune(s1), []rune(s2), 0)
+		if actualScore != score {
+			t.Fatalf(
+				"s1=%#v, s2=%#v, score=%v, actualScore=%v, distance=%v",
+				s1, s2, score, actualScore,
+				distance(s1, s2, buff),
+			)
+		}
+	}
+	test("signature", "signature", 200)
+	test("signature", "sigature", 177)
+	test("signature", "signatory", 155)
+	// test("", "", )
+}
