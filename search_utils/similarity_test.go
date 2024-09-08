2a32f8b36f3ed8144b3e5f68343b24aea8652d1e
Saeed Rasooli <saeed.gnu@gmail.com>
2024-09-08 05:02:16 +0330

    search_utils/similarity_test.go: do not import levenshtein

--- search_utils/similarity_test.go
+++ search_utils/similarity_test.go
@@ -2,13 +2,7 @@ package search_utils
 
 import (
 	"testing"
-
-	"github.com/ilius/go-dict-commons/levenshtein"
 )
-
-func distance(a string, b string, buff []uint16) int {
-	return levenshtein.ComputeDistance([]rune(a), []rune(b), buff)
-}
 
 func TestSimilarity1(t *testing.T) {
 	buff := make([]uint16, 100)
@@ -16,9 +10,8 @@ func TestSimilarity1(t *testing.T) {
 		actualScore := Similarity([]rune(s1), []rune(s2), buff, 0)
 		if actualScore != score {
 			t.Fatalf(
-				"s1=%#v, s2=%#v, score=%v, actualScore=%v, distance=%v",
+				"s1=%#v, s2=%#v, score=%v, actualScore=%v",
 				s1, s2, score, actualScore,
-				distance(s1, s2, buff),
 			)
 		}
 	}
