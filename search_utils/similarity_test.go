c4477b185766bbfde6ad6b6cbc3888ccf56ec4e2
Saeed Rasooli <saeed.gnu@gmail.com>
2025-03-12 11:15:13 +0330

    add more tests for Similarity func

--- search_utils/similarity_test.go
+++ search_utils/similarity_test.go
@@ -18,5 +18,25 @@ func TestSimilarity1(t *testing.T) {
 	test("signature", "signature", 200)
 	test("signature", "sigature", 177)
 	test("signature", "signatory", 155)
-	// test("", "", )
+	test("", "hello", 0)
+	test("hello", "", 0)
+	test("hello", "hello", 200)
+	test("ab", "aa", 0)
+	test("ab", "ba", 0)
+	test("ab", "aaa", 0)
+	test("bbb", "a", 0)
+	test("kitten", "sitting", 0)
+	test("distance", "difference", 0)
+	test("levenshtein", "frankenstein", 0)
+	test("resume and cafe", "resumes and cafes", 176)
+	test("a very long string that is meant to exceed", "another very long string that is meant to exceed", 175)
+	test("signature", "sigature", 177)
+	test("resumé and café", "resumés and cafés", 176)
+	test("resume and cafe", "resumé and café", 173)
+	test("Hafþór Júlíus Björnsson", "Hafþor Julius Bjornsson", 165)
+
+	// Only 2 characters are less in the 2nd string
+	test("།་གམ་འས་པ་་མ།", "།་གམའས་པ་་མ", 177)
+
+	// test("", "", 0)
 }
