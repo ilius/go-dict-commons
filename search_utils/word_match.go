4e031ee3e06581ae14849dc332d2b12767d06ccb
Saeed Rasooli <saeed.gnu@gmail.com>
2025-09-25 06:45:54 +0330

    search_utils: add ScoreWordMatch with test

---
+++ search_utils/word_match.go
@@ -0,0 +1,87 @@
+package search_utils
+
+import (
+	"strings"
+)
+
+// ScoreWordMatch returns a score based on exact word match between query words and term words
+func ScoreWordMatch(terms []string, query string) uint8 {
+	queryWords := strings.Split(query, " ")
+	if len(queryWords) == 1 {
+		// optmization, should not change the result
+		return scoreWordMatchSingleWordQuery(terms, query)
+	}
+	queryWordMap := map[string]int{}
+	for wordIndex, word := range queryWords {
+		queryWordMap[word] = wordIndex
+	}
+	bestScore := 0
+	for termI, termOrig := range terms {
+		term := strings.ToLower(termOrig)
+		termWords := strings.Split(term, " ")
+		termWordMap := map[string]bool{}
+		score := -termI
+		for wordIndex, word := range termWords {
+			termWordMap[word] = true
+			queryWordIndex, ok := queryWordMap[word]
+			if !ok {
+				score--
+				continue
+			}
+			if queryWordIndex > wordIndex {
+				score += 10 - (queryWordIndex - wordIndex)
+			} else {
+				score += 10 - (wordIndex - queryWordIndex)
+			}
+		}
+		for word := range queryWordMap {
+			if !termWordMap[word] {
+				score--
+			}
+		}
+		if score < 0 {
+			continue
+		}
+		if score > bestScore {
+			bestScore = score
+		}
+	}
+	if bestScore <= 0 {
+		return 0
+	}
+	outputScore := 200 - 10*len(queryWordMap) + bestScore
+	if outputScore > 200 {
+		return 200
+	}
+	return uint8(outputScore)
+}
+
+// query is one word
+func scoreWordMatchSingleWordQuery(terms []string, query string) uint8 {
+	bestScore := 0
+	for termI, termOrig := range terms {
+		term := strings.ToLower(termOrig)
+		score := -termI
+		for wordIndex, word := range strings.Split(term, " ") {
+			if word != query {
+				score--
+				continue
+			}
+			score += 10 - wordIndex
+		}
+		if score < 0 {
+			continue
+		}
+		if score > bestScore {
+			bestScore = score
+		}
+	}
+	if bestScore <= 0 {
+		return 0
+	}
+	outputScore := 190 + bestScore
+	if outputScore > 200 {
+		return 200
+	}
+	return uint8(outputScore)
+}
---
+++ search_utils/word_match_test.go
@@ -0,0 +1,38 @@
+package search_utils
+
+import "testing"
+
+func TestScoreWordMatch(t *testing.T) {
+	test := func(terms []string, query string, score uint8) {
+		actualScore := ScoreWordMatch(terms, query)
+		if actualScore != score {
+			t.Errorf("terms=%#v, query=%#v, score=%v, actualScore=%v", terms, query, score, actualScore)
+			t.Fail()
+		}
+	}
+	test([]string{"test"}, "test", 200)
+	test([]string{"test"}, "test 123", 189)
+	test([]string{"test"}, "112 test", 188)
+	test([]string{"test"}, "test 123 x", 178)
+	test([]string{"test"}, "x test abc", 177)
+	test([]string{"test"}, "test 1 2 3 4 5", 145)
+	test([]string{"test"}, "x", 0)
+	test([]string{"test"}, "hello", 0)
+	test([]string{"test"}, " ", 0)
+	test([]string{"test"}, "hello world", 0)
+
+	test([]string{"test word"}, "test word", 200)
+	test([]string{"test word"}, "test", 199)
+	test([]string{"test word"}, "word", 198)
+	test([]string{"test word"}, "test 123", 188)
+	test([]string{"test word"}, "word 123", 187)
+	test([]string{"test word"}, "112 test", 187)
+	test([]string{"test word"}, "x test abc", 176)
+	test([]string{"test word"}, "test word 1 2 3 4 5", 145)
+	test([]string{"test word"}, "hello", 0)
+	test([]string{"test word"}, " ", 0)
+	test([]string{"test word"}, "hello world", 0)
+
+	test([]string{"x", "test word"}, "test word", 199)
+	test([]string{"x", "y", "test word"}, "test word", 198)
+}
