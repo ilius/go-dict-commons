50bc153aa0f8a2376a9a5c61e6c942f0e249b6bd
Saeed Rasooli <saeed.gnu@gmail.com>
2025-09-26 20:21:19 +0330

    ScoreFuzzy: rename a loop var, and add some comments

--- search_utils/fuzzy.go
+++ search_utils/fuzzy.go
@@ -13,6 +13,7 @@ type ScoreFuzzyArgs struct {
 
 // ScoreFuzzy ...
 // Make sure you don't use the same buff in multiple goroutines
+// returns a number between 50 and 200
 func ScoreFuzzy(
 	terms []string,
 	args *ScoreFuzzyArgs,
@@ -21,27 +22,29 @@ func ScoreFuzzy(
 	bestScore := uint8(0)
 	queryMainWord := args.QueryMainWord
 	mainWordIndex := args.MainWordIndex
-	for termI, termOrig := range terms {
+	for termIndex, termOrig := range terms {
+		// subtract: to give better scores for terms that come first
+		// after 3rd term, they are all treated the same
 		subtract := uint8(3)
-		if termI < 3 {
-			subtract = uint8(termI)
+		if termIndex < 3 {
+			subtract = uint8(termIndex)
 		}
 		term := strings.ToLower(termOrig)
-		if term == args.Query {
+		if term == args.Query { // exact match, done
 			return 200 - subtract
 		}
 		words := strings.Split(term, " ")
-		if len(words) < args.MinWordCount {
+		if len(words) < args.MinWordCount { // term has too few words, skip
 			continue
 		}
 		score := Similarity(args.QueryRunes, []rune(term), buff, subtract)
 		if score > bestScore {
 			bestScore = score
-			if score >= 180 {
+			if score >= 180 { // term is good enough without looking at its words
 				continue
 			}
 		}
-		if len(words) == 1 {
+		if len(words) == 1 { // no need to look at the words
 			continue
 		}
 		bestWordScore := uint8(0)
@@ -52,23 +55,27 @@ func ScoreFuzzy(
 			}
 			if wordI == mainWordIndex {
 				wordScore -= 1
+				// just to differentiate between word match and whole term match
 			} else {
+				// matching word's position is not the same as query, %10 penalty
 				wordScore -= wordScore / 10
 			}
 			if wordScore > bestWordScore {
 				bestWordScore = wordScore
 			}
 		}
-		if bestWordScore < 50 {
+		if bestWordScore < 50 { // don't waste any more time on this term
 			continue
 		}
 		if args.QueryWordCount > 1 {
+			// Fastest way to make an int become about 2/3 of its current value
+			// in this case about %64 because 1/2 + 1/7 ~= 0.642
+			// Tip: Python prioritizes + over >> so you need to add ()
 			bestWordScore = bestWordScore>>1 + bestWordScore/7
 		}
 		if bestWordScore > bestScore {
 			bestScore = bestWordScore
 		}
-
 	}
 	return bestScore
 }
